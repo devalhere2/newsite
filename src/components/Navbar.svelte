fcfca8a0f7d9c91d5be2aa3f9d585b79a7504b1d
deval patel <deval22990@gmail.com>
2023-11-02 10:09:18 +0530

    initial commit

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,41 @@
+<div class=Navbar>
+    <div class="Navbar_logo">
+        <img src="./marsf.png" alt="" style="height: 50px;">
+        <h2 style="margin: 0; padding: 0; color: #27077D ; font-family: monospace;">deval.me</h2>
+    </div>
+    <div class="Navbar_links">
+        <div class="links"> AI Models</div>
+        <div class="links"> File Manipulations</div>
+        <div class="links"> Resume</div>
+    </div>
+</div>
+
+<style>
+    .Navbar {
+        display: flex;
+        align-items: center;
+        padding-top: 10px;
+        padding-bottom: 5px;
+        border-bottom: 3px solid #27077D;
+        border-radius: 5px;
+    }
+    .Navbar_links{
+        display: flex;
+        margin-right: 10vw;
+    }
+    .Navbar_logo {
+        display: flex;
+        align-items: end;
+        margin-left: 8vw;
+        flex-grow: 1;
+        
+    }
+    .links{
+        font-family: monospace;
+        font-size: large;
+        margin-left: 20px;
+        margin-right: 20px;
+        font-weight: bold;
+        color: rgba(0, 128, 0, 0.74);
+    }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Navbar from '../components/Navbar.svelte'
+</script>
+
+<Navbar />
+<slot />
+
+<style>
+    :global(body) {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+	
+</style>
